7eaab4e9d2bf843eefe27cf94e533ab93fd09a3e
Ondrej Vrsan <OndrejV2@kentico.com>
2018-02-16 13:10:24 +0100

    PLATFORM-11397 - Updated assembly info of Publisher

--- Kentico.AcceleratedMobilePages.Publisher/Properties/AssemblyInfo.cs
+++ Kentico.AcceleratedMobilePages.Publisher/Properties/AssemblyInfo.cs
@@ -9,12 +9,12 @@ using CMS;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Kentico.AcceleratedMobilePages.Publisher")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Kentico Accelerated Mobile Pages Publisher")]
+[assembly: AssemblyDescription("Kentico Accelerated Mobile Pages Publisher")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Kentico.AcceleratedMobilePages.Publisher")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyCompany("Kentico software s.r.o.")]
+[assembly: AssemblyProduct("AcceleratedMobilePages")]
+[assembly: AssemblyCopyright("Copyright © 2018 Kentico software s.r.o.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -36,5 +36,5 @@ using CMS;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("11.0.0.0")]
+[assembly: AssemblyFileVersion("11.0.0.0")]
